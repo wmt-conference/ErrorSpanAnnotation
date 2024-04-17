06986a959a7937ddcf5b2dcd965b009e86569bbf
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-04-17 12:10:19 +0200

    user level span analysis

--- ESA/analysis_gemba.py
+++ ESA/analysis_gemba.py
@@ -2,12 +2,12 @@ from annotations import AppraiseAnnotations
 import numpy as np
 import collections
 
-anno_gemba = AppraiseAnnotations(f"campaign-ruction-rc5/240315rc5GEMBA.scores.csv", "GEMBA").generate_scores().df
-
+anno = AppraiseAnnotations(f"campaign-ruction-rc5/240315rc5GEMBA.scores.csv", "GEMBA").generate_scores()
+anno = anno.df.merge(anno.annotator_mapping, left_on="login", right_on="login", how="left")
 
 to_average_users = []
-for login in anno_gemba.login.unique():
-    df = anno_gemba[anno_gemba["login"] == login]
+for login in anno.AnnotatorID.unique():
+    df = anno[anno["AnnotatorID"] == login]
     v_time = list(df["start_time"])
     v_time.sort()
     v_time = [b-a for a, b in zip(v_time, v_time[1:])]
@@ -43,13 +43,73 @@ for login in anno_gemba.login.unique():
     to_average_users.append({k:np.average(v) for k,v in to_average.items()})
 
 to_average_users.sort(key=lambda x: x["time"])
-for user_i, user in enumerate(to_average_users):
+for user in to_average_users:
     print(
         f"{user['time']:.1f}s",
         f"{user['span_orig']:.1f}",
         f"{user['span_kept']:.1f}",
         f"{user['span_removed']:.1f}",
         f"{user['span_added']:.1f}",
+        f"{user['span_final']:.1f}",
+        sep = " & ",
+        end=" \\\\\n"
+    )
+
+print(r"\midrule")
+# hack to average
+user = {k:np.average([x[k] for x in to_average_users]) for k in user.keys()}
+print(
+    f"{user['time']:.1f}s",
+    f"{user['span_orig']:.1f}",
+    f"{user['span_kept']:.1f}",
+    f"{user['span_removed']:.1f}",
+    f"{user['span_added']:.1f}",
+    f"{user['span_final']:.1f}",
+    sep = " & ",
+    end=" \\\\\n"
+)
+    
+
+anno = AppraiseAnnotations(f"campaign-ruction-rc5/240315rc5ESA.scores.csv", "ESA").generate_scores()
+anno = anno.df.merge(anno.annotator_mapping, left_on="login", right_on="login", how="left")
+
+to_average_users = []
+for login in anno.AnnotatorID.unique():
+    df = anno[anno["AnnotatorID"] == login]
+    v_time = list(df["start_time"])
+    v_time.sort()
+    v_time = [b-a for a, b in zip(v_time, v_time[1:])]
+    v_time_median, v_time_quantile = np.median(v_time), np.quantile(v_time, 0.9)
+
+    prev_time = 0
+    to_average = collections.defaultdict(list)
+    for _, row in df.iterrows():
+        if type(row["span_errors"]) is float:
+            continue
+
+        v_time = row["start_time"]-prev_time
+        if v_time > v_time_quantile or v_time <= 0:
+            v_time = v_time_median
+        prev_time = row["start_time"]
+        to_average["time"].append(v_time)
+        to_average["span_final"].append(len(row["span_errors"]))
+
+    to_average_users.append({k:np.average(v) for k,v in to_average.items()})
+
+to_average_users.sort(key=lambda x: x["time"])
+for user in to_average_users:
+    print(
+        f"{user['time']:.1f}s",
+        f"{user['span_final']:.1f}",
+        sep = " & ",
+        end=" \\\\\n"
+    )
+
+print(r"\midrule")
+# hack to average
+user = {k:np.average([x[k] for x in to_average_users]) for k in user.keys()}
+print(
+        f"{user['time']:.1f}s",
         f"{user['span_final']:.1f}",
         sep = " & ",
         end=" \\\\\n"
