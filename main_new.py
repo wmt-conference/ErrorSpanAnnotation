0a2c4f7e771becc6da26d1b3a4b7bf40cd47f165
Tom Kocmi <tomkocmi@microsoft.com>
2024-06-06 21:33:32 +0200

    removed tmp

--- main_new.py
+++ main_new.py
@@ -6,11 +6,10 @@ from ESA.annotation_loader import AnnotationLoader
 
 def main(args):
     # class containing all information
-    annotations = AnnotationLoader(refresh_cache=True)
+    annotations = AnnotationLoader(refresh_cache=False)
 
     df = annotations.get_view(["MQM-1", "LLM", "ESA-1", "ESAAI-1", "ESA-2", "ESAAI-2", "WMT-MQM", "WMT-DASQM"])
 
-    df.to_excel("full_frame.xlsx")
 
 if __name__ == '__main__':
     app.run(main)
