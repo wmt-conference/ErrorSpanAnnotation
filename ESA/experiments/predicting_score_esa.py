61172ec92cd7b2a30fe196b4e5d8765e80ecf25c
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-06-11 12:09:30 +0200

    add normalized MQM comparison

--- ESA/experiments/predicting_score_esa.py
+++ ESA/experiments/predicting_score_esa.py
@@ -1,4 +1,5 @@
 import collections
+import numpy as np
 import scipy.stats
 import matplotlib.pyplot as plt
 import ESA.figutils
@@ -6,34 +7,16 @@ from ESA.annotation_loader import AnnotationLoader
 df = AnnotationLoader(refresh_cache=False).get_view(["LLM", "ESA-1", "MQM-1", "ESA-2"], only_overlap=False).dropna()
 ESA.figutils.matplotlib_default()
 
-
-def featurize_line(item, protocol):
-    return (
-        item["sourceID"],
-        item[f"ESA-1_score"],
-    ), (
-        len([x for x in item[f"{protocol}_error_spans"] if x["severity"] == "minor"]),
-        len([x for x in item[f"{protocol}_error_spans"] if x["severity"] == "major"]),
-        len([x for x in item[f"{protocol}_error_spans"] if x["end_i"] == "missing"]),
-    )
-
-
 # compute model performance
 # -------------------------
-data_raw = [featurize_line(r, "ESA-1") for _, r in df.iterrows()]
-data = collections.defaultdict(list)
-for item in data_raw:
-    data[item[0][0]].append((item[1], item[0][1]))
-data = list(data.values())
-
-data_true = [x[1] for l in data for x in l]
+data_true = [r["ESA-1_score"] for _, r in df.iterrows()]
 prev_max = None
 corrs = []
 for fv in range(0, 100+1, 1):
     fv = fv / 10
-    def predict_mqm_like_custom(spans):
-        return -sum([{"minor": 1, "major": fv, "undecided": 0}[x["severity"]] for x in spans])
-    data_pred_lr = [predict_mqm_like_custom(r["ESA-1_error_spans"]) for _, r in df.iterrows()]
+    def predict_mqm_like_custom(item):
+        return -sum([{"minor": 1, "major": fv, "undecided": 0}[x["severity"]] for x in item["ESA-1_error_spans"]])/len(item["hypothesis"].split())
+    data_pred_lr = [predict_mqm_like_custom(r) for _, r in df.iterrows()]
     corr = scipy.stats.pearsonr(data_pred_lr, data_true)[0]
     if prev_max is None or corr > prev_max:
         print(f"1-{fv:0>4}->ESA1: {corr:.8f} *")
@@ -55,6 +38,27 @@ plt.xlabel("minor: -1, major: -$x$ error weights")
 plt.tight_layout()
 plt.savefig("PAPER_ESA/generated_plots/predicting_score.pdf")
 plt.show()
+
+# compute system-level correlation
+data_sys_true = collections.defaultdict(list)
+data_sys_plain = collections.defaultdict(list)
+data_sys_normalized = collections.defaultdict(list)
+def predict_mqm_like_plain(item):
+        return -sum([{"minor": 1, "major": 5, "undecided": 0}[x["severity"]] for x in item["ESA-1_error_spans"]])
+def predict_mqm_like_normalized(item):
+        return -sum([{"minor": 1, "major": 5, "undecided": 0}[x["severity"]] for x in item["ESA-1_error_spans"]])/len(item["hypothesis"].split())
+for _, r in df.iterrows():
+    data_sys_true[r["systemID"]].append(r["ESA-1_score"])
+    data_sys_plain[r["systemID"]].append(predict_mqm_like_plain(r))
+    data_sys_normalized[r["systemID"]].append(predict_mqm_like_normalized(r))
+
+data_sys_true = {k: np.average(v) for k, v in data_sys_true.items()}
+data_sys_plain = {k: np.average(v) for k, v in data_sys_plain.items()}
+data_sys_normalized = {k: np.average(v) for k, v in data_sys_normalized.items()}
+assert data_sys_true.keys() == data_sys_plain.keys()
+print("Plain     ", f"{scipy.stats.spearmanr(list(data_sys_true.values()), list(data_sys_plain.values()))[0]:.4f}")
+print("Normalized", f"{scipy.stats.spearmanr(list(data_sys_true.values()), list(data_sys_normalized.values()))[0]:.4f}")
+
 
 
 # compute feature importance
