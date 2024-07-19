d41f92894df98bee7dac299b216567c0ebb5856d
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-07-19 10:34:20 +0200

    WMT24: implement quality control

--- preparation/wmt24/prepare_batches_main.py
+++ preparation/wmt24/prepare_batches_main.py
@@ -6,20 +6,24 @@ import collections
 import random
 import itertools
 import copy
-
-SEC_TUTORIAL = json.load(open(f"data/tutorial/de-en.esa.json", "r"))
+import quality_control
 
 args = argparse.ArgumentParser()
 args.add_argument("--langs", default="en-de")
 args.add_argument("--wave", type=int, default=0)
 args = args.parse_args()
 
+SEC_TUTORIAL = json.load(open(f"data/tutorial/de-en.esa.json", "r"))
+SYSTEMS = json.load(open(f"data/wmt24_general/systems.json", "r"))[args.langs]
+SYSTEMS = [sys.replace("IOL-Research", "IOL_Research") for sys in SYSTEMS if sys != "tutorial"]
+BAD_COUNT = 12
+
 
 def load_tsv(filename: str, tsv_index: None | int = None):
     data = open(filename, "r").readlines()
 
     # all files should start with canary
-    assert data[0].split()[0].lower()== "canary", data[0]
+    assert data[0].split()[0].lower() == "canary", data[0]
     data = data[1:]
 
     data = [x.strip("\n") for x in data]
@@ -29,23 +33,34 @@ def load_tsv(filename: str, tsv_index: None | int = None):
 
     return data
 
+
 lines_src = load_tsv(f"data/wmt24_general/sources/{args.langs}.txt")
-lines_doc = load_tsv(f"data/wmt24_general/documents/{args.langs}.docs", tsv_index=1)
-lines_domain = load_tsv(f"data/wmt24_general/documents/{args.langs}.docs", tsv_index=0)
+lines_doc = load_tsv(
+    f"data/wmt24_general/documents/{args.langs}.docs", tsv_index=1)
+lines_domain = load_tsv(
+    f"data/wmt24_general/documents/{args.langs}.docs", tsv_index=0)
 
 # Python supports this!
 assert len(lines_src) == len(lines_doc) == len(lines_domain)
 
 # load systems and references
-systems = [os.path.basename(f).removesuffix(".txt") for f in glob.glob(f"data/wmt24_general/system-outputs/{args.langs}/*.txt")]
-lines_systems = {system: load_tsv(f"data/wmt24_general/system-outputs/{args.langs}/{system}.txt") for system in systems}
-references = [os.path.basename(f).removesuffix(".txt").removeprefix(f"{args.langs}.") for f in glob.glob(f"data/wmt24_general/references/{args.langs}.*.txt")]
-lines_systems |= {ref: load_tsv(f"data/wmt24_general/references/{args.langs}.{ref}.txt") for ref in references}
+lines_systems = {
+    sys: load_tsv(f"data/wmt24_general/system-outputs/{args.langs}/{sys}.txt")
+    for sys in SYSTEMS
+}
+references = [
+    os.path.basename(f).removesuffix(".txt").removeprefix(f"{args.langs}.")
+    for f in glob.glob(f"data/wmt24_general/references/{args.langs}.*.txt")
+]
+lines_systems |= {
+    ref: load_tsv(f"data/wmt24_general/references/{args.langs}.{ref}.txt")
+    for ref in references
+}
 # treat references as systems from now on
-systems += references
+SYSTEMS += references
 del references
 
-assert all(len(lines_systems[system]) == len(lines_src) for system in systems)
+assert all(len(lines_systems[system]) == len(lines_src) for system in SYSTEMS)
 
 # we need stable ordering of docs
 docs = list(set(lines_doc))
@@ -57,12 +72,12 @@ docs = docs[args.wave*len(docs)//2:(args.wave+1)*len(docs)//2]
 doc_word_count = collections.Counter()
 for doc, line_src in zip(lines_doc, lines_src):
     doc_word_count[doc] += len(line_src.split())
-
 doc_line_count = collections.Counter(lines_doc)
 
-print(f"DOCS ({len(docs)}) in wave {args.wave}:", docs)
+print(f"DOCS ({len(docs)}, {sum([doc_line_count[doc] for doc in docs])} lines) in wave {args.wave}:", docs)
 
-sys_docs = list(itertools.product(docs, systems))
+# try to balance document lengths (words)
+sys_docs = list(itertools.product(docs, SYSTEMS))
 sys_docs.sort(key=lambda x: doc_word_count[x[0]])
 sys_docs_long = sys_docs[:len(sys_docs)//2]
 sys_docs_short = sys_docs[len(sys_docs)//2:]
@@ -77,50 +92,102 @@ docs_queue = sys_docs_short + sys_docs_long
 docs_queue[0::2] = sys_docs_short
 docs_queue[1::2] = sys_docs_long
 
-print(f"SYSTEMS ({len(systems)})")
-print(f"DOC+SYSTEMS ({len(docs_queue)})")
+print(f"SYSTEMS ({len(SYSTEMS)})")
+print(f"DOC+SYSTEMS ({len(docs_queue)}, {sum([doc_line_count[doc] for doc, sys in docs_queue if doc in docs])} lines)")
 
 tasks = []
+R_DOC_FILLING = random.Random(0)
+R_DOC_SHUFFLING = random.Random(0)
 while docs_queue:
-    task = []
-    task += copy.deepcopy(SEC_TUTORIAL)
-    while len(task) < 100:
+    task_docs = [copy.deepcopy(SEC_TUTORIAL)]
+    def task_docs_len(): return sum([len(l) for l in task_docs])
+    def task_qc_len(): return sum([len([x for x in l if x["itemType"] == "BAD"]) for l in task_docs])
+
+    while task_docs_len() < 100 - BAD_COUNT:
+        # we ran out of documents to distribute
         if not docs_queue:
-            print(f"Unfinished task  but ran out of documents so I'm leaving this task partially unfinished ({len(task)}). TODO")
-            # TODO: remove me, this is incorrect
-            task = task + task[-(100 - len(task)):]
+            duplicate_i = 0
+
+            # sample new doc (not tutorial, not QC --- can't be because they're added later)
+            # blocks the same doc from being dupped twice?
+            task_docs_available = copy.deepcopy(task_docs[1:])
+            R_DOC_FILLING.shuffle(task_docs_available)
+
+            while task_docs_len() < 100 - BAD_COUNT:
+                doc_new = task_docs_available.pop(0)
+                # trim it if it's too much
+                doc_new = doc_new[:100 - BAD_COUNT - task_docs_len()]
+                for line in doc_new:
+                    line["documentID"] = f"{line['documentID']}#dup"
+                duplicate_i += 1
+                task_docs.append(doc_new)
+                # could happen that we need to duplicate a duplication
+                # luckily the document ID would now be #dup#dup
+                task_docs_available.append(copy.deepcopy(doc_new))
+
+            # end the filling phase
             break
         
-        # TODO: add quality control
-        # TODO: handle multimodal (speech domain) differently
-
-        if doc_line_count[docs_queue[0][0]] + len(task) > 100:
-            print(f"The next document would be too much so I'm leaving this task partially unfinished ({len(task)}). TODO")
-            # TODO: remove me, this is incorrect
-            task = task + task[-(100 - len(task)):]
+        # if the next document would be too much then just trim it
+        if doc_line_count[docs_queue[0][0]] + task_docs_len() > 100 - BAD_COUNT:
+            doc, system = docs_queue[0]
+            lines_i = [i for i, line in enumerate(lines_doc) if line == doc]
+            item_lines_src = [lines_src[i] for i in lines_i]
+            item_lines_tgt = [lines_systems[system][i] for i in lines_i]
+            task_docs.append([
+                {
+                    "documentID": doc + "#incomplete",
+                    "sourceText": line_src,
+                    "targetText": line_tgt,
+                    "itemType": "TGT",
+                    "TODO": "TODO other keys",
+                }
+                for line_src, line_tgt in zip(item_lines_src, item_lines_tgt)
+            ][:100-BAD_COUNT-task_docs_len()])
+            # end the filling phase
             break
+        
+
+        # TODO: handle multimodal (speech domain) differently
 
         doc, system = docs_queue.pop(0)
         lines_i = [i for i, line in enumerate(lines_doc) if line == doc]
         item_lines_src = [lines_src[i] for i in lines_i]
         item_lines_tgt = [lines_systems[system][i] for i in lines_i]
-        task += [
+        task_docs.append([
             {
                 "documentID": doc,
                 "sourceText": line_src,
                 "targetText": line_tgt,
+                "itemType": "TGT",
                 "TODO": "TODO other keys",
             }
             for line_src, line_tgt in zip(item_lines_src, item_lines_tgt)
-        ]
-    tasks.append(task)
+        ])
+
+    # add quality control
+    while task_docs_len() < 100:
+        task_docs.append(quality_control.create_bad_document(task_docs))
+    # trim the last doc to 100
+    task_docs[-1] = task_docs[-1][:100 - (task_docs_len()-len(task_docs[-1]))]
+
+    # final shuffle all docs except for tutorial
+    task_docs = [task_docs[0]]+R_DOC_SHUFFLING.sample(task_docs[1:], len(task_docs[1:]))
+
+    tasks.append([x for l in task_docs for x in l])
 
 print("\n".join([
     f"Task {task_i:>3}: "
     f"{len(set([x['documentID'] for x in task])):>4} docs, "
     f"{len(task):>6} lines, "
+    f"{sum([len([x for x in task if x['documentID'].endswith('#bad')])]):>6} BAD lines, "
+    f"{sum([len([x for x in task if x['documentID'].endswith('#incomplete')])]):>6} incomplete doc lines, "
+    f"{sum([len([x for x in task if x['itemType'] == 'TGT' and '#dup' not in x['documentID']])]):>6} unique TGT lines, "
     f"{sum([len(x['sourceText'].split()) for x in task]):>6} words"
     for task_i, task in enumerate(tasks)
 ]))
 
-json.dump(tasks, open(f"data/wmt24_general/batches_wave{args.wave}.{args.langs}.json", "w"))
+json.dump(
+    tasks,
+    open(f"data/wmt24_general/batches_wave{args.wave}.{args.langs}.json", "w")
+)
---
+++ preparation/wmt24/quality_control.py
@@ -0,0 +1,143 @@
+import random
+import copy
+
+LANG_2_TO_3 = {
+    "en": "eng",
+    "de": "deu",
+    "cs": "ces",
+    "hr": "hrv",
+    "pl": "plk",
+    "ru": "rus",
+    "zh": "zho",
+}
+
+RANDOM_SAMPLE_BAD = random.Random(0)
+
+
+def create_bad_document(data_docs):
+    """
+    Randomly sample a document from a list and corrupt it.
+    """
+    all_tgt = [x["targetText"] for l in data_docs for x in l]
+
+    docs_available = [
+        doc for doc in data_docs
+        if not doc[0]["documentID"].endswith("#dup") and doc[0]["itemType"] != "BAD"
+    ]
+    doc_bad = copy.deepcopy(RANDOM_SAMPLE_BAD.choice(docs_available))
+
+    for obj in doc_bad:
+        sents = []
+        for sent in obj["targetText"].split(". "):
+            # don't corrupt if it's short sentence (could be a non-sentences)
+            if len(sent.split()) < 5:
+                sents.append(sent)
+            else:
+                corrupted, start_i, end_i = corrupt_text_by_mixing(
+                    obj["targetText"],
+                    RANDOM_SAMPLE_BAD.choice(all_tgt),
+                    character_based=False
+                )
+                sents.append(corrupted)
+        obj["targetText"] = ". ".join(sents)
+        obj["itemType"] = "BAD"
+        # no need to add a unique identifier because there's at most one bad document per good one
+        obj["documentID"] = obj["documentID"] + f"#bad"
+
+    return doc_bad
+
+
+RANDOM_GEN_BAD = random.Random(0)
+
+
+def corrupt_text_by_mixing(seg_orig: str, seg_inject: str, character_based: bool = False) -> str:
+    """
+    Creates bad reference for given text.
+
+    Segment length (a, b] to phrase length (excluding a, including b)
+    mapping defined as follows:
+        ( 0,   1] : 1
+        ( 1,   5] : 2
+        ( 5,   8] : 3
+        ( 8,  15] : 4
+        (15,  20] : 5
+        (20, max] : 6
+
+    For character-based languages, which do not support tokenisation
+    by whitespace, the resulting phrase length will be doubled, and
+    is interpreted as a character length.
+    """
+
+    if character_based:
+        seg_data = list(seg_orig)
+        ref_data = list(seg_inject)
+    else:
+        seg_data = seg_orig.split(' ')
+        ref_data = seg_inject.split(' ')
+
+    seg_len = len(seg_data)
+    ref_len = len(ref_data)
+
+    # Determine length of bad phrase, relative to segment length.
+    _seg_to_bad_mapping = {
+        (None, 1): 2,
+        (1, 5): 2,
+        (5, 8): 3,
+        (8, 15): 4,
+        (15, 20): 5,
+        (20, None): 6,
+    }
+
+    bad_len = 0
+    for seg_pair in _seg_to_bad_mapping:
+        left, right = seg_pair
+
+        # seg_len == right; left edge case
+        if not left:
+            if seg_len == right:
+                bad_len = _seg_to_bad_mapping[seg_pair]
+                break
+
+        # left < seg_len; right edge case
+        elif not right:
+            if left < seg_len:
+                bad_len = _seg_to_bad_mapping[seg_pair]
+                break
+
+        # left < seg_len <= right; middle cases
+        elif left < seg_len <= right:
+            bad_len = _seg_to_bad_mapping[seg_pair]
+            break
+
+    # Double length of bad phrase for character-based languages.
+    if character_based:
+        bad_len = 2 * bad_len
+
+    # Determine random replacement position. For segments longer than
+    # (bad_len + 1), we enforce that this cannot be sentence initial
+    # or final, so positions 0 and (seg_len - bad_len -1) are invalid
+    # and we use an embedded bad_pos in [1, (seg_len - bad_len - 1)].
+    # This happens for all seg_len > 3.
+    bad_pos = 0
+    if seg_len - bad_len > 0:
+        bad_pos = RANDOM_GEN_BAD.choice(range(seg_len - bad_len))
+
+    elif seg_len > 3:
+        _xs = max(1, seg_len - bad_len - 1)
+        bad_pos = RANDOM_GEN_BAD.choice([x + 1 for x in range(_xs)])
+
+    ref_pos = 0
+    if ref_len - bad_len > 0:
+        ref_pos = RANDOM_GEN_BAD.choice(range(ref_len - bad_len))
+
+    bad_data = (
+        seg_data[:bad_pos]
+        + ref_data[ref_pos: ref_pos + bad_len]
+        + seg_data[bad_pos + bad_len:]
+    )
+    bad_text = ' '.join(bad_data)
+    if character_based:
+        bad_text = ''.join(bad_data)
+
+    # return text but also indicies
+    return (bad_text, bad_pos, bad_len)
