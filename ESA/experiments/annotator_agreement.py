f7bc86bce24b0be2e8143c9c945a824791b8375f
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-08-02 15:02:59 +0200

    compute IAAs

--- ESA/experiments/annotator_agreement.py
+++ ESA/experiments/annotator_agreement.py
@@ -6,6 +6,8 @@ import scipy.stats
 def mqm_like_score(spans):
 	return sum([{"critical": 25, "major": 5, "minor": 1, "undecided": 0}[x["severity"]] for x in spans])
 
+print("Inter annotator agreement")
+
 for protocol in ["ESA", "ESAAI"]:
     corr = scipy.stats.spearmanr(df[protocol + "-1_score"], df[protocol + "-2_score"])[0]
     print(
@@ -15,6 +17,26 @@ for protocol in ["ESA", "ESAAI"]:
 	)
     scores_1 = [mqm_like_score(x) for x in df[protocol + "-1_error_spans"]]
     scores_2 = [mqm_like_score(x) for x in df[protocol + "-2_error_spans"]]
+    corr = scipy.stats.spearmanr(scores_1, scores_2)[0]
+    print(
+        f"{protocol:>10} (MQM-like)",
+        f"{corr:.3f}",
+        sep=" & ", end=" \\\\\n"
+	)
+    
+
+print("Intra annotator agreement")
+df = AnnotationLoader(refresh_cache=False).get_view(["ESA-1", "ESAAI-1", "ESA-IAA", "ESAAI-IAA"], only_overlap=True).dropna()
+
+for protocol in ["ESA", "ESAAI"]:
+    corr = scipy.stats.spearmanr(df[protocol + "-1_score"], df[protocol + "-IAA_score"])[0]
+    print(
+        f"{protocol:>10} (score)   ",
+        f"{corr:.3f}",
+        sep=" & ", end=" \\\\\n"
+	)
+    scores_1 = [mqm_like_score(x) for x in df[protocol + "-1_error_spans"]]
+    scores_2 = [mqm_like_score(x) for x in df[protocol + "-IAA_error_spans"]]
     corr = scipy.stats.spearmanr(scores_1, scores_2)[0]
     print(
         f"{protocol:>10} (MQM-like)",
